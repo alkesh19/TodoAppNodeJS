76964713ccf9b4ae31bb62a5419235c92fd5ac79
alkeshthakor19 <alkeshathakor@gmail.com>
2023-08-27 06:44:46 +0530

    Todo app created using file system.
    Changes:
    1. Todo app created using file system.
    2. Get route created to fetch notes from file system.
    3. createTodo route created to create new note.
    4. Note created and fetched from frontend.
    
    Signed-off-by: alkeshthakor19 <alkeshathakor@gmail.com>

---
+++ TodoFileSystem/todoServerFileSystem.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const { error } = require("console");
+const { removeAllListeners } = require("process");
+const path = require("path");
+//const cors = require("cors");
+const port = 3000
+
+const app = express();
+  
+app.use(bodyParser.json());
+//app.use(cors());
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
+
+function findIndex(arr, id) {
+    for(let id = 0; i< arr.length; i++) {
+        if(arr[i].id === id) return i;
+    }
+    return -1;
+}
+
+function removeAtIndex(arr,index) {
+    let newArray = [];
+    for(let i=0; i<arr.length; i++) {
+        if(i !== index) newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+app.get("/getTodos",(req,res) => {
+   fs.readFile("todo.json","utf-8",(error,data) => {
+       if(error) throw error;
+       res.json(JSON.parse(data));
+   });
+});
+
+app.post("/createTodo",(req,res) => {
+    const newTodo = {
+        id: Math.floor(Math.random() * 1000000),
+        title: req.body.title,
+        description: req.body.description
+    };
+
+    fs.readFile("todo.json","utf-8", (error,data) => {
+        if(error) throw error;
+        const todos = JSON.parse(data);
+        todos.push(newTodo);
+        fs.writeFile("todo.json", JSON.stringify(todos), (error,data) => {
+            if(error) throw error;
+            res.status(200).json(newTodo);
+        });
+
+    })
+})
+
+/* app.delete("/deleteTodos/:id",(req,res)=>{
+   fs.readFile("todo.json","utf-8",(error,data) => {
+        if(error) throw error;
+        const todos = JSON.parse(data);
+        const todoIndex = findIndex(todos,parseInt(req.params.id));
+        if(todoIndex === -1) {
+            req.status(404).send();
+        } else {
+            todos = removeAtIndex(todos,todoIndex);
+        }
+   });
+}); */
+
+function onAppStart() {
+    console.log("Sever is running on port 3000");
+}
+app.listen(port,onAppStart);
+
+
